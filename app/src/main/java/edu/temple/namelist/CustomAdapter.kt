b9fbeef041df81b55a9d6fb2eba42314bad932ee
bictorg <vj.jimeneznunez@gmail.com>
2024-10-09 14:49:39 -0400

    Create updateData method to set names and notify the change

--- app/src/main/java/edu/temple/namelist/CustomAdapter.kt
+++ app/src/main/java/edu/temple/namelist/CustomAdapter.kt
@@ -6,11 +6,11 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 
-class CustomAdapter(private val names: List<String>, private val context: Context) : BaseAdapter() {
+class CustomAdapter(private var names: List<String>, private val context: Context) : BaseAdapter() {
 
     // How many items are in the collection
     override fun getCount(): Int {
-        return 5
+        return names.size
     }
 
     // Fetch an item from the collection
@@ -35,5 +35,11 @@ class CustomAdapter(private val names: List<String>, private val context: Contex
         }.apply {
             text = getItem(p0).toString()
         }
+    }
+
+    // We update the data and notify the adapter that the data has changed
+    fun updateData(newNames: List<String>) {
+        names = newNames
+        notifyDataSetChanged()
     }
 }
